7db25f4a4f2c78848ef156ba880ba419d2a0a806
Dani <dmartimarti@gmail.com>
2018-10-12 11:20:41 +0100

    new comments added to the code

--- test_2_single_pass.py
+++ test_2_single_pass.py
@@ -3,19 +3,22 @@ This code should transfer 50 uL between two positions
 '''
 from opentrons import labware, instruments
 
-plate = labware.load('96-flat', '1')
+# load labware
+plate = labware.load('96-flat', '1') # loads a 96-flat plate
+tiprack = labware.load('tiprack-200ul', '2') # loads a tip rack of 200 uL 
+pipette = instruments.P300_Single(mount = 'right') # loads the P300 single on the right arm
 
-tiprack = labware.load('tiprack-200ul', '2')
 
-pipette = instruments.P300_Single(mount = 'right')
+#### actions
 
+# pick up a tip from tip rack at position A2
 pipette.pick_up_tip(tiprack.wells('A2'))
-
+# aspirate 50 uL from position B1
 pipette.aspirate(50, plate.wells('B1'))
-
+# dispense 50 uL to position A1
 pipette.dispense(50, plate.wells('A1'))
-pipette.blow_out()
+pipette.blow_out() # blow out the remaining liquid
 
 
 # drop tip on trash
-pipette.drop_tip()     
+pipette.return_tip()
